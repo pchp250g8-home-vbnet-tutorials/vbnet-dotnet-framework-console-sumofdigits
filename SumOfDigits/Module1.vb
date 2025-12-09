01930b6c2c9ea80ff2312ec386fb63fbbe81e48a
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-09 12:09:03 +0300

    Добавьте файлы проекта.

---
+++ SumOfDigits/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Input an Integer Number:")
+        Dim strNum = Console.ReadLine()
+        Dim iNum As Long = 0
+        Int64.TryParse(strNum, iNum)
+        Dim nSumDigits = 0 : Dim iVal = Math.Abs(iNum)
+        Do While (iVal > 0)
+            nSumDigits += iVal Mod 10
+            iVal \= 10
+        Loop
+        Console.WriteLine("The sum of digits of the number {0} is {1}", iNum, nSumDigits)
+        Console.Read()
+    End Sub
+
+End Module
---
+++ SumOfDigits/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("SumOfDigits")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SumOfDigits")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("44c52107-906e-45fb-87ba-411ef4823554")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
